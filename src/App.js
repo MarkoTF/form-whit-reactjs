af1a6ac4d3cbbd5ea876a758a8a41ba2a4ae8f63
Marco <marco.com17@gmail.com>
2021-06-10 11:57:35 -0500

    Funciona las alertas, falata la notificacion

--- src/App.js
+++ src/App.js
@@ -10,44 +10,40 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      // datos:{
-	// cliente_nombre: {
-	  // contenido: '',
-	  // status: 'normal'
-	// },
-	// cliente_nombre: {
-	  // contenido: '',
-	  // status: 'normal'
-	// },
-	// cliente_opotunidades: {
-	  // contenido: '',
-	  // status: 'normal'
-	// },
-	// cliente_objetivo_general: {
-	  // contenido: '',
-	  // status: 'normal'
-	// },
-	// cliente_espectativas: {
-	  // contenido: '',
-	  // status: 'normal'
-	// },
-	// cliente_ventajas: {
-	  // contenido: '',
-	  // status: 'normal'
-	// },
-	// marca_propuesta_valor: '',
-	// marca_identidad_corporativa: '',
-	// marca_engagement: '',
-      // },
-      cliente_nombre: '',
-      cliente_opotunidades: '',
-      cliente_objetivo_general: '',
-      cliente_espectativas: '',
-      cliente_ventajas: '',
-      marca_propuesta_valor: '',
-      marca_identidad_corporativa: '',
-      marca_engagement: '',
-
+      datos:{
+	cliente_nombre: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	cliente_opotunidades: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	cliente_objetivo_general: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	cliente_espectativas: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	cliente_ventajas: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	marca_propuesta_valor: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	marca_identidad_corporativa: {
+	  contenido: '',
+	  status: 'normal'
+	},
+	marca_engagement: {
+	  contenido: '',
+	  status: 'normal'
+	},
+      },
       isModalOpen: false,
     }
 
@@ -58,18 +54,57 @@ class App extends React.Component {
 
   handleInputChange(e) {
     const nombre = e.target.name;
+    const valor = e.target.value;
+    const datos = {...this.state.datos}; //Para clonar y no mutar
     this.setState({
-      [nombre]: e.target.value,
+      datos: {
+	...datos,
+	[nombre]: {
+	  contenido: valor,
+	  status: 'normal'
+	}
+      },
     });
   }
 
   handleSubmit(e) {
     const camposLlenos = revisarCampos(e.target);
 
+    //console.log(this.state.datos);
     if(camposLlenos) {
-      console.log(this.state);
+      console.log(this.state.datos);
       this.handleModal();
     } else {
+      const datos = {...this.state.datos};
+      let nuevosDatos = {};
+
+      for(let i = 0; i < e.target.length - 1; i++) {
+	let nombre = e.target[i].name;
+
+	if(e.target[i].value === '') {
+	  nuevosDatos = {
+	    ...nuevosDatos, 
+	    [nombre]: {
+	      contenido: datos.[nombre].contenido,
+	      status: 'vacio'
+	    }
+	  }
+	} else {
+	  nuevosDatos = {
+	    ...nuevosDatos, 
+	    [nombre]: {
+	      contenido: datos.[nombre].contenido,
+	      status: 'lleno'
+	    }
+	  }
+	}
+      }
+
+      //console.log(nuevosDatos);
+      this.setState({
+	datos: nuevosDatos
+      });
+      console.log(this.state.datos);
       console.log('llenar todos los campos')
     }
 
@@ -83,8 +118,6 @@ class App extends React.Component {
   }
 
   render() {
-
-    const help = <p className="help is-danger px-3">This email is invalid</p>;
 
     return (
       <div className="App">
@@ -101,7 +134,8 @@ class App extends React.Component {
 	    <div className="box p-5">
 	      <CuadroContenido 
 		name='cliente_nombre'
-		help={help}
+		status={this.state.datos.cliente_nombre.status}
+		value={this.state.datos.cliente_nombre.contenido}
 		onChange={this.handleInputChange}
 		encabezado="Nombre del cliente" 
 		descripcion="Descripción breve acerca del cliente, su historia, servicios y productos"/>
@@ -109,6 +143,8 @@ class App extends React.Component {
 	      <div className="columns my-0">
 		<div className="column pr-0 py-0">
 		  <CuadroContenido 
+		    status={this.state.datos.cliente_opotunidades.status}
+		    value={this.state.datos.cliente_opotunidades.contenido}
 		    onChange={this.handleInputChange}
 		    name='cliente_opotunidades'
 		    encabezado="Declaración de área de oportunidad" 
@@ -117,6 +153,8 @@ class App extends React.Component {
 		<div className="column pl-0 py-0">
 		  <CuadroContenido
 		    name='cliente_objetivo_general'
+		    status={this.state.datos.cliente_objetivo_general.status}
+		    value={this.state.datos.cliente_objetivo_general.contenido}
 		    onChange={this.handleInputChange}
 		    encabezado='Objetivo general del proyecto'
 		    descripcion='(debe ser preciso y corto) Ejemplo: Aumentar el número de suscriptores a servicio'/>
@@ -125,12 +163,16 @@ class App extends React.Component {
 
 	      <CuadroContenido
 		name='cliente_espectativas'
+		status={this.state.datos.cliente_espectativas.status}
+		value={this.state.datos.cliente_espectativas.contenido}
 		onChange={this.handleInputChange}
 		encabezado="Espectativas" 
 		descripcion="1. Expectativa primera&#10;2. Expectativa segunda&#10;3. Expectativa tercera&#10;4. Expectativa cuarta&#10;5. Expectativa quinta..."/>
 
 	      <CuadroContenido
 		name='cliente_ventajas'
+		status={this.state.datos.cliente_ventajas.status}
+		value={this.state.datos.cliente_ventajas.contenido}
 		onChange={this.handleInputChange}
 		encabezado="Ventajas competitivas (mencionadas por el cliente)" 
 		descripcion="1.&#10;2.&#10;3.&#10;4.&#10;5...."/>
@@ -142,18 +184,24 @@ class App extends React.Component {
 	    <div className="box p-5">
 	      <CuadroContenido
 		name='marca_propuesta_valor'
+		status={this.state.datos.marca_propuesta_valor.status}
+		value={this.state.datos.marca_propuesta_valor.contenido}
 		onChange={this.handleInputChange}
 		encabezado="Estrategia de branding: propuesta de valor" 
 		descripcion="Factores que identifican a la marca ante su competencia. Pueden ser relacionados con servicios, frases, historia... todo aquello que el cliente considere que le da un valor agregado a su marca."/>
 
 	      <CuadroContenido 
 		name='marca_identidad_corporativa'
+		status={this.state.datos.marca_identidad_corporativa.status}
+		value={this.state.datos.marca_identidad_corporativa.contenido}
 		onChange={this.handleInputChange}
 		encabezado="Estrategia de branding: identidad corporativa" 
 		descripcion="En esta sección se agrega el logo, las fuentes tipográficas, las paleta de colores, la iconografía y todas las referencia a productos existentes, como sitio web, documentos, etc."/>
 
 	      <CuadroContenido
 		name='marca_engagement'
+		status={this.state.datos.marca_engagement.status}
+		value={this.state.datos.marca_engagement.contenido}
 		onChange={this.handleInputChange}
 		encabezado="Estrategia de branding: engagement" 
 		descripcion="En esta sección se agregan elementos implementados dentro del producto, como iconos o frases e imágenes que fomenten la interacción del usuario con la marca."/>
--- src/components/CuadroContenido.js
+++ src/components/CuadroContenido.js
@@ -1,17 +1,27 @@
 function CuadroContenido(props) {
+
+  let status;
+  if(props.status === 'vacio') {
+    status = <p className="help is-danger px-4">Segurate de llenar este campo</p>;
+  } else if(props.status === 'lleno') {
+    status = <p className="help is-success px-4">Este campo ha sido llenado correctamente</p>;
+  } else {
+    status = '';
+  }
+
   return(
     <div className="card my-2 px-1">
       <div className="card-header border-b">
-	<div className="card-header-title is-family-secondary">{props.encabezado}</div>
+	<div className="card-header-title is-family-secondary pb-1">{props.encabezado}</div>
       </div>
-
+      {status}
       <div className="card-content px-1 py-1">
-       {props.help}
        <textarea 
-	  className="textarea has-fixed-size" 
+	  className="textarea has-fixed-size pt-0" 
 	  onChange={props.onChange}
 	  name={props.name} 
 	  placeholder={props.descripcion} 
+	  value={props.value}
 	  rows="10"></textarea> 
       </div>
     </div>
